eac8d7927c3148620584f140c8632deb463200e0
davidalexandercampbell <davidacampbell@yahoo.com>
2020-04-12 11:48:01 +0100

    adding 2nd example

--- test_runner.py
+++ test_runner.py
@@ -27,3 +27,23 @@ def testRunnerRegistersResult(self):
 
         runner.run(unittest.TestSuite())
         self.assertEqual(self.wasRegistered, 1) 
+        
+        def test_startTestRun_stopTestRun_called(self):
+        class LoggingTextResult(LoggingResult):
+            separator2 = ''
+            def printErrors(self):
+                pass
+
+        class LoggingRunner(unittest.TextTestRunner):
+            def __init__(self, events):
+                super(LoggingRunner, self).__init__(StringIO())
+                self._events = events
+
+            def _makeResult(self):
+                return LoggingTextResult(self._events)
+
+        events = []
+        runner = LoggingRunner(events)
+        runner.run(unittest.TestSuite())
+        expected = ['startTestRun', 'stopTestRun']
+        self.assertEqual(events, expected) 
